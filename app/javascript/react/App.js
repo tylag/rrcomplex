fe473abeca6a8052917043f849ba1cea2db3ade7
tylag <toshimasayasunori@gmail.com>
2022-09-13 04:24:31 +1000

    Added react entry point

---
+++ app/javascript/react/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+
+export default function App(){
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={() => { setCount(count + 1) }}>Increase</button>
+      <button onClick={() => { setCount(count - 1) }}>Decrease</button>
+    </>
+  )
+}
