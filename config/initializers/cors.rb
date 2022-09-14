e817eb00cdb8ebdd8b11cb65740183b84e37e508
tylag <toshimasayasunori@gmail.com>
2022-09-14 21:25:01 +1000

    Devise interface added for auth api

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', 
+      headers: :any, 
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], 
+      methods: [:get, :post, :patch, :put]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/auth'
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
